092b6c6ad74a88cdcf991945d1ddaee5dff53251
Steve VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-02-05 09:22:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,7 @@ const { Person } = require('./models');
 const { Op } = require('sequelize');
 
 const main = async () => {
-  try {
-    const person = await Person.findByPk(18);
-    console.log(person.name);
-
-  } catch(e) {
-    console.log(e);
-  }
+  // write your queries here
 }
 const run = async () => {
   await main();
